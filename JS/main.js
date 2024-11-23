ca9f3b7dfc68f26f990eac4c4f220cc9092b698d
eslamgh86 <eslamgomaa1986@gmail.com>
2024-11-23 22:18:30 +0200

    I added the author name

--- JS/main.js
+++ JS/main.js
@@ -1,12 +1,20 @@
 
-var listQuotes = ["Be yourself; everyone else is already taken.", "So many books, so little time", "A room without books is like a body without a soul.", "Be who you are and say what you feel, because those who mind don't matter, and those who matter don't mind.", "You only live once, but if you do it right, once is enough.", "In three words I can sum up everything I've learned about life: it goes on.", "If you tell the truth, you don't have to remember anything", "A friend is someone who knows all about you and still loves you."]
+var listQuotes = 
+[" \"Be yourself; everyone else is already taken\" <br />--Epictetus", 
+  " \"So many books, so little time\" <br />--Nelson Mandela", 
+  " \"A room without books is like a body without a soul\" <br />--Frank Sinatra", 
+  " \"Be who you are and say what you feel, because those who mind don't matter, and those who matter don't mind\" <br />--Frank Sinatra", 
+  " \"You only live once, but if you do it right, once is enough \" <br />--Elbert Hubbard",
+   " \"In three words I can sum up everything I've learned about life: it goes on\" <br />--Elbert Hubbard", 
+   "\"If you tell the truth, you don't have to remember anything\" <br />--Elbert Hubbard", 
+   "\"A friend is someone who knows all about you and still loves you\" <br />--Epictetus"]
 var lastQuote;  //for comparison in the next loop
 
 
 function generatorQuotes() {
 
   for (var i = 0; i < listQuotes.length; i++) {
-    var numSelected = Math.floor(Math.random() * listQuotes.length); // In the loop to inusre iteration 
+    var numSelected = Math.floor(Math.random() * listQuotes.length); // In the loop to inusre iteration   
     var newQuote = listQuotes[numSelected]; // selection from list Array
     
     if (newQuote !== lastQuote) 
